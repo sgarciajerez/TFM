939d9129c23fb7ae2580b672867cd2a34ca68312
Santi Garcia <sgarciajerez@gmail.com>
2023-02-25 13:29:26 +0100

    probando generador de enlaces

---
+++ linea.php
@@ -0,0 +1,22 @@
+<?php> <php>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="CSS/fonts.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,400&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="Images/logoParaTittle.png" type="image/x-icon">
+    <title>LINEA AAA</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
---
+++ scripts/generarEnlace.js
@@ -0,0 +1,13 @@
+const SELECTOR_LINEAS = document.getElementById('linea');
+
+SELECTOR_LINEAS.addEventListener ('change', () => {
+    let idLinea = SELECTOR_LINEAS.value;
+    (function generarEnlace (){
+        let url = new URL('http://127.0.0.1:5500?line=1');
+        let params = new URLSearchParams(url.search);
+        params.append('line', idLinea);
+    })();
+});
+
+
+
